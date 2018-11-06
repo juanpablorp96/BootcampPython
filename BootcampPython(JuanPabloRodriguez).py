f64f790afb385dd0fd2d2b144669299120b412c4
Juan Pablo <juanrodriguezp96@gmail.com>
2018-11-05 20:37:17 -0500

    Quinto Commit

--- BootcampPython(JuanPabloRodriguez).py
+++ BootcampPython(JuanPabloRodriguez).py
@@ -1,7 +1,16 @@
 import time
+import sys
 
 
-class Contacto:
+# PEP 8 checked
+# Requirements: at least two classes
+class User:
+    def __init__(self, name, contacts):
+        self.name = name
+        self.contacts = contacts
+
+
+class Contact:
 
     def __init__(self, name, last_name, age, email, phone_number):
         self.name = name
@@ -21,30 +30,41 @@ class Contacto:
 
 l_contacts = []
 l_hidden = []
-l_contacts.append(Contacto("juan", "restrepo", 22, "juan@globant.com", {"house": "3542134", "cell phone": "3204532478"}))
-time.sleep(2)
-l_contacts.append(Contacto("Carlos", "Lopez", 20, "carlos@globant.com", {"office": "2345673", "cell phone": "3156385721"}))
-time.sleep(2)
-l_contacts.append(Contacto("Lina", "Pulido", 30, "lina@globant.com", {"personal": "1234567", "cell phone": "3005732146"}))
+# Requirements: Files & List comprehension
+# File have some contacts to be load in the project
+data = [line.strip() for line in open("/home/juan-pablo/Documentos/contacts_file.txt", 'r')]
+
+for i in data:
+    dat = i.split(',')
+    l_contacts.append(Contact(dat[0], dat[1], dat[2], dat[3], {dat[4]: dat[5]}))
 
 
+# Function sign_up create an instance of User class
+def sign_up():
+    print("Welcome! \n Please enter your name")
+    name = input()
+    user = User(name, l_contacts)
+
+
+# Function menu show the functions of the application and have the logic
 def menu():
     print("Contacts App \n"
           "1. List contacts (sorted by creation date) \n"
           "2. Create new contact \n"
           "3. Update existing contact \n"
           "4. Hide contact \n"
-          "5. Recover hidden contacts \n \n"
+          "5. Recover hidden contacts \n"
+          "6. Exit \n \n"
           "Please enter the number...")
     choice = input()
-
-    if(choice == "1"):
+# Sort the list of contacts by the date, using lambda function
+    if choice == "1":
         l_contacts.sort(key=lambda x: x.date, reverse=False)
         for i in l_contacts:
             print(i.__repr__())
         menu()
-
-    if(choice == "2"):
+# Receives data by console and create an instance of Contact class and append in the contacts list
+    if choice == "2":
         print("Enter name:")
         name = input()
         print("Enter last name:")
@@ -58,25 +78,25 @@ def menu():
         print("Enter phone number for " + phone_number_name)
         phone_number = input()
 
-        l_contacts.append(Contacto(name, last_name, int(age), email, {phone_number_name: phone_number}))
+        l_contacts.append(Contact(name, last_name, int(age), email, {phone_number_name: phone_number}))
         flag = True
-
-        while(flag == True):
+# Special case, user can add any numbers for an specific contact
+        while flag:
             print("Other phone number? \n 1. Yes  2. No")
             choice = input()
-            if(choice == "1"):
+            if choice == "1":
                 print("Enter phone number name (office, personal, cell phone, etc): \n")
                 phone_number_name = input()
                 print("Enter phone number for " + phone_number_name)
                 phone_number = input()
                 l_contacts[len(l_contacts) - 1].phone_number[phone_number_name] = phone_number
 
-            if(choice == "2"):
+            if choice == "2":
                 flag = False
-
+        print("Successful create contact")
         menu()
-
-    if(choice == "3"):
+# Update an specific attribute of a given contact
+    if choice == "3":
         count = 1
         print("Choose contact")
         for i in l_contacts:
@@ -87,37 +107,35 @@ def menu():
         print("Choose option to update \n 1. Name \n 2. Last Name \n 3. Age \n 4. Email \n 5. Numbers")
         choice = input()
 
-        if(choice == "1"):
+        if choice == "1":
             print('Current name --> {}'.format(l_contacts[int(contact_update) - 1].name))
             print("Enter new name")
             new_name = input()
             l_contacts[int(contact_update) - 1].name = new_name
             print("Successful update")
 
-        if(choice == "2"):
+        if choice == "2":
             print('Current last name --> {}'.format(l_contacts[int(contact_update) - 1].last_name))
             print("Enter new last name")
             new_last_name = input()
             l_contacts[int(contact_update) - 1].last_name = new_last_name
             print("Successful update")
 
-
-        if(choice == "3"):
+        if choice == "3":
             print('Current age --> {}'.format(l_contacts[int(contact_update) - 1].age))
             print("Enter new age")
             new_age = input()
             l_contacts[int(contact_update) - 1].age = int(new_age)
             print("Successful update")
 
-
-        if(choice == "4"):
+        if choice == "4":
             print('Current email --> {}'.format(l_contacts[int(contact_update) - 1].email))
             print("Enter new email")
             new_email = input()
             l_contacts[int(contact_update) - 1].email = new_email
             print("Successful update")
 
-        if(choice == "5"):
+        if choice == "5":
             count = 1
             print("Current numbers: \n")
             for key, val in l_contacts[int(contact_update) - 1].phone_number.items():
@@ -127,14 +145,15 @@ def menu():
             num = input()
             count = 1
             for key in l_contacts[int(contact_update) - 1].phone_number:
-                if(int(num) == count):
+                if int(num) == count:
                     print("Enter new phone number")
                     new_phone_number = input()
                     l_contacts[int(contact_update) - 1].phone_number[key] = new_phone_number
                 count += 1
+            print("Successful update")
         menu()
-
-    if(choice == "4"):
+# "Hide" a given contact, pop and specific contact and append it in a "hidden contacts list"
+    if choice == "4":
         count = 1
         print("Choose contact")
         for i in l_contacts:
@@ -145,14 +164,17 @@ def menu():
         print("Successful hide \n")
 
         menu()
-
-    if(choice == "5"):
+# Recover the hidden contacts from the "hidden contacts list" to the original contacts list
+    if choice == "5":
         for i in l_hidden:
             l_contacts.append(i)
         print("Successful recover of hidden contacts")
 
         menu()
+# Exit the program
+    if choice == "6":
+        sys.exit()
 
-#comprehension_list = [ e**2 for e in l_contacts if e.name]
 
+sign_up()
 menu()
